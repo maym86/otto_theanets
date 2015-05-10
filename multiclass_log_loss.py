6e7b04a748ba3e58913aa64a43dc3a5901957a10
Michael May <maym86@gmail.com>
2015-05-10 13:12:21 -0700

    Added log loss function and test set.

--- multiclass_log_loss.py
+++ multiclass_log_loss.py
@@ -1,1 +1,36 @@
+from __future__ import division
 __author__ = 'Michael May'
+
+import scipy as sp
+
+def log_loss(act, pred):
+    epsilon = 1e-15
+    pred = sp.maximum(epsilon, pred)
+    pred = sp.minimum(1-epsilon, pred)
+    ll = sum(act*sp.log(pred) + sp.subtract(1,act)*sp.log(sp.subtract(1,pred)))
+    ll = ll * -1.0/len(act)
+    return ll
+
+def multiclass_log_loss(classes_np, prediction_np):
+
+    classes = classes_np.tolist()
+    prediction = prediction_np.tolist()
+
+    class_array = []
+    class_count = len(set(classes))
+    for i in classes:
+        row = [0] * class_count
+        row[i] = 1
+        class_array.append(row)
+
+    scores = []
+    for index in range(0, len(prediction)):
+        result = log_loss(class_array[index], prediction[index])
+        scores.append(result)
+
+    return sum(scores) / len(scores)
+
+
+
+if __name__ == '__main__':
+    main()
--- theanets_test.py
+++ theanets_test.py
@@ -5,7 +5,7 @@ from sklearn import cross_validation
 import pandas as pd
 import numpy as np
 from sklearn import preprocessing
-
+from multiclass_log_loss import *
 # try scaling http://sebastianraschka.com/Articles/2014_about_feature_scaling.html
 
 
@@ -13,15 +13,15 @@ def load_test_data(std_scale):
     raw_testing_data = pd.read_csv('test.csv')
     raw_testing_data = raw_testing_data.astype('float32')  # convert types to float32
 
-    #Get the features and the classes
+    # Get the features and the classes
     test_features = raw_testing_data.iloc[:, 1:94].values  #get the feature vectors
 
-    test_features = std_scale.transform(test_features) #scale the features
+    test_features = std_scale.transform(test_features)  #scale the features
     return test_features
 
 
 def save_results(file, test_features, results):
-    #create colums and headers
+    # create colums and headers
     index = range(1, len(test_features) + 1)
     columns = ['Class_1', 'Class_2', 'Class_3', 'Class_4', 'Class_5', 'Class_6', 'Class_7', 'Class_8', 'Class_9']
 
@@ -40,7 +40,7 @@ def class_to_int(class_string):
 def load_training_data():
     raw_training_data = pd.read_csv('train.csv')
 
-    #convert types to ints
+    # convert types to ints
     raw_training_data['target'] = raw_training_data['target'].apply(class_to_int)
     raw_training_data = raw_training_data.astype('float32')
     raw_training_data['target'] = raw_training_data['target'].astype('int32')
@@ -52,27 +52,33 @@ def load_training_data():
     print np.unique(classes)
 
     #split train/validate
-    feat_train, feat_val, class_train, class_val = cross_validation.train_test_split(features, classes, test_size=0.3,
+    feat_train, feat_test, class_train, class_test = cross_validation.train_test_split(features, classes, test_size=0.3,
+                                                                                       random_state=0)
+
+    feat_train, feat_val, class_train, class_val = cross_validation.train_test_split(feat_train, class_train,
+                                                                                     test_size=0.3,
                                                                                      random_state=0)
 
     #scale the features
     std_scale = preprocessing.StandardScaler().fit(feat_train)
     feat_train = std_scale.transform(feat_train)
     feat_val = std_scale.transform(feat_val)
+    feat_test = std_scale.transform(feat_test)
 
     #convert to np array for theanets
     training_data = [feat_train, class_train]
     validation_data = [feat_val, class_val]
+    test_data = [feat_test, class_test]
 
-    return training_data, validation_data, std_scale
+    return training_data, validation_data, test_data, std_scale
 
 
 def main():
-    training_data, validation_data, std_scale = load_training_data()
+    training_data, validation_data, test_data, std_scale = load_training_data()
     climate.enable_default_logging()
 
     trainers = ['sgd', 'nag', 'rprop', 'rmsprop', 'adadelta', 'esgd', 'hf', 'sample', 'layerwise', 'pretrain']
-    #layers = [(93, 256, 128, 9), (93, 128, 64, 32, 9)]
+    # layers = [(93, 256, 128, 9), (93, 128, 64, 32, 9)]
     layers = [(93, 256, 128, 9)]
     for l in layers:
         exp = theanets.Experiment(
@@ -82,23 +88,31 @@ def main():
 
         #loop through trainers
         for t in trainers:
-            train_result = exp.train(training_data,
-                validation_data,
-                algorithm=t,
-                hidden_l1=0.001,
-                weight_inverse=0,
-                train_batches=300,
-                monitors={'hid1.out': (-0.9, -0.1, 0.1, 0.9)}
-                )
 
-            print train_result
-            print train_result[1]['acc'] #validation accuracy
-            out_file = 'experimenting/' + str(train_result[1]['acc']) + t + str(l)
+            train_result = exp.train(training_data,
+                                     validation_data,
+                                     algorithm=t,
+                                     hidden_l1=0.001,
+                                     train_batches=300
+                                     )
+
+
+            #get an prediction of the accuracy from the test_data
+            test_results = exp.network.predict(test_data[0])
+            loss = multiclass_log_loss(test_data[1], test_results)
+
+            print 'validation acc:', train_result[1]['loss']  #validation accuracy
+            print 'test loss:', loss
+
+
+            out_file = 'log_loss/' + str(loss) + t + str(l)
             exp.save(out_file + '.pkl')
 
-            test_features = load_test_data(std_scale)
-            results = exp.network.predict(test_features)
-            save_results(out_file + '.csv', test_features, results)
+
+            #save the kaggle results
+            kaggle_test_features = load_test_data(std_scale)
+            results = exp.network.predict(kaggle_test_features)
+            save_results(out_file + '.csv', kaggle_test_features, results)
 
 
 if __name__ == "__main__":
