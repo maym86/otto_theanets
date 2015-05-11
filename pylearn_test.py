27ba69b58a129a49813a01729410ee3d2ab09734
gleesonm <mike.gleeson-may@here.com>
2015-05-11 11:25:37 -0700

    Fixed pylearn2 example.

--- pylearn_test.py
+++ pylearn_test.py
@@ -6,15 +6,24 @@ from pylearn2.models import mlp
 from pylearn2.training_algorithms import sgd
 from pylearn2.termination_criteria import EpochCounter
 from pylearn2.datasets.dense_design_matrix import DenseDesignMatrix
-import numpy as np
-
 
 from sklearn import cross_validation
 import pandas as pd
-import numpy as np
 from sklearn import preprocessing
 from ml_metrics import *
 # try scaling http://sebastianraschka.com/Articles/2014_about_feature_scaling.html
+
+
+class Dataset(DenseDesignMatrix):
+    def __init__(self, features, classes):
+        self.class_names = set(classes)
+
+        actual = np.zeros([features.shape[0], len(set(classes))])
+        rows = actual.shape[0]
+        actual[np.arange(rows), classes.astype(int)] = 1
+
+        print actual.shape
+        super(Dataset, self).__init__(X=features, y=actual)
 
 
 def load_test_data(std_scale):
@@ -63,66 +72,58 @@ def load_training_data():
     feat_train, feat_test, class_train, class_test = cross_validation.train_test_split(features, classes, test_size=0.3,
                                                                                        random_state=0)
 
-    feat_train, feat_val, class_train, class_val = cross_validation.train_test_split(feat_train, class_train,
-                                                                                     test_size=0.3,
-                                                                                     random_state=0)
-
     #scale the features
     std_scale = preprocessing.StandardScaler().fit(feat_train)
     feat_train = std_scale.transform(feat_train)
-    feat_val = std_scale.transform(feat_val)
     feat_test = std_scale.transform(feat_test)
 
-    #convert to np array for theanets
-    training_data = [feat_train, class_train]
-    validation_data = [feat_val, class_val]
+    training_data = Dataset(feat_train, class_train)
     test_data = [feat_test, class_test]
 
-    return training_data, validation_data, test_data, std_scale
+    return training_data, test_data, std_scale
 
 
 def main():
-    training_data, validation_data, test_data, std_scale = load_training_data()
+    training_data, test_data, std_scale = load_training_data()
     kaggle_test_features = load_test_data(std_scale)
 
-    trainers = ['nag', 'sgd', 'rprop', 'rmsprop', 'adadelta', 'esgd', 'hf', 'sample', 'layerwise', 'pretrain']
-    layers = [(93, 256, 128, 9), (93, 300, 200, 9), (93, 128, 64, 32, 9)]
 
-    for l in layers:
-        for t in trainers:
+    #pylearn2 ML
+    hidden_layer = mlp.Sigmoid(layer_name='hidden', dim=93, irange=.1, init_bias=1.)
+    # create Softmax output layer
+    output_layer = mlp.Softmax(9, 'output', irange=.1)
+    # create Stochastic Gradient Descent trainer that runs for 400 epochs
+    trainer = sgd.SGD(learning_rate=.05, batch_size=10, termination_criterion=EpochCounter(400))
+    layers = [hidden_layer, output_layer]
+    # create neural net that takes two inputs
+    ann = mlp.MLP(layers, nvis=93)
+    trainer.setup(ann, training_data)
+    # train neural net until the termination criterion is true
+    count = 0
+    while True:
+        trainer.train(dataset=training_data)
+        ann.monitor.report_epoch()
+        ann.monitor()
+        count += 1
+        if not trainer.continue_learning(ann):
+            break
 
-            hidden_layer = mlp.Sigmoid(layer_name='hidden', dim=2, irange=.1, init_bias=1.)
-            # create Softmax output layer
-            output_layer = mlp.Softmax(2, 'output', irange=.1)
-            # create Stochastic Gradient Descent trainer that runs for 400 epochs
-            trainer = sgd.SGD(learning_rate=.05, batch_size=10, termination_criterion=EpochCounter(400))
-            layers = [hidden_layer, output_layer]
-            # create neural net that takes two inputs
-            ann = mlp.MLP(layers, nvis=2)
-            trainer.setup(ann, training_data)
-            # train neural net until the termination criterion is true
-            while True:
-                trainer.train(dataset=training_data)
-                ann.monitor.report_epoch()
-                ann.monitor()
-                if not trainer.continue_learning(ann):
-                    break
+    #get an prediction of the accuracy from the test_data
+    test_results = ann.fprop(theano.shared(test_data[0], name='test_data')).eval()
+
+    print test_results.shape
+    loss = multiclass_log_loss(test_data[1], test_results)
+
+    print 'Test multiclass log loss:', loss
+
+    out_file = 'pylearn2_results/' + str(loss) + 'ann'
+    #exp.save(out_file + '.pkl')
 
 
-            #get an prediction of the accuracy from the test_data
-            test_results = ann.fprop(theano.shared(test_data[0], name='inputs')).eval()
-            loss = multiclass_log_loss(test_data[1], test_results)
+    #save the kaggle results
 
-            print 'Test multiclass log loss:', loss
-
-            out_file = 'results/' + str(loss) + t + str(l)
-            exp.save(out_file + '.pkl')
-
-
-            #save the kaggle results
-
-            results = ann.fprop(theano.shared(kaggle_test_features).eval())
-            save_results(out_file + '.csv', kaggle_test_features, results)
+    results = ann.fprop(theano.shared(kaggle_test_features, name='kaggle_test_data')).eval()
+    save_results(out_file + '.csv', kaggle_test_features, results)
 
 
 if __name__ == "__main__":
